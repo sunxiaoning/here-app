2c7d6e2706cdbb5d519bcc40066145d634738f03
William <1530358579@qq.com>
2017-10-22 22:12:24 +0800

    测试数据

--- app/view/map/Map.js
+++ app/view/map/Map.js
@@ -18,17 +18,16 @@ Ext.define('here.view.map.Map', {
         markerTap: true,
         locationFinish : false,
         data: [
-                    { lng: '121.538089', lat: '31.22488', name: '上海东昌' }, 
-                    { lng: '121.540577', lat: '31.224127', name: '信用保险' },
-                    { lng: '121.538784', lat: '31.223483', name: '上海乐赚' },
-                    { lng: '121.539494', lat: '31.222437', name: '上海浦东银行' }
-              ],
+                    { lng: '121.538089', lat: '31.22488', name: '上海东昌',address : '上海市浦东新区峨山路91弄160号陆家嘴软件园'}, 
+                    { lng: '121.538784', lat: '31.223483', name: '上海乐赚',address:'上海市浦东新区峨山路91弄98号陆家嘴软件园' },
+                    { lng: '121.48393', lat: '31.291559', name: '虹口区教师进修学院',address:'水电路839'},
+                    { lng: '121.486409', lat: '31.292679', name: '智慧桥创业园',address:'上海市虹口区广灵四路116号-1号楼-203'}
+              ]
     },
-    //点击坐标处理
-    onTapMarker: function (me, marker) {
-        //创建信息窗口
-        var infoWindow = new BMap.InfoWindow(marker.options.name);
-        marker.openInfoWindow(infoWindow);
+    
+    // 点击坐标处理
+    onTapMarker : function (me, marker) {
+        Ext.Msg.confirm("选择我的位置", "选中位置："+marker.options.name+","+marker.options.address, Ext.emptyFn);
     },
 
     initialize: function(){
@@ -93,8 +92,8 @@ Ext.define('here.view.map.Map', {
         }
         else {
             myLocation = {
-                lng:114.3146600000,
-                lat:34.8017770000
+                lng:121.484947,
+                lat:31.29175
             };
             me.setCenter(myLocation);
            // markData.push(myLocation);
