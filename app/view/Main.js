e4ce1828bff16e5cc4b2e82d8fa49e5bacdf65e5
William <1530358579@qq.com>
2017-10-22 22:13:53 +0800

    home main view 调整

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,42 +1,20 @@
 Ext.define('here.view.Main', {
-    extend: 'Ext.Panel',
+    extend: 'Ext.NavigationView',
     xtype: 'main',
 	config : {
-		layout: {
-            type: 'card'
-        },
+		defaultBackButtonText : '返回',
+        autoDestroy: false,
 		items:[
 			{
-				xtype : 'toolbar',
-				docked: 'top',
-
-				items : [
-					{
-						border : 0,
-						iconCls : 'locate',
-						style: 'background:none',
-						html:'Here'
-					},
-					{ xtype: 'spacer' },
-					{
-						xtype: 'searchfield',
-						placeholder: '搜索'
-					}
-					
-				]
-			},
-			{
-				
 				xtype: 'tabpanel',
+				title : 'Here',
 				tabBarPosition : 'bottom',
 				items: [
 						{
 							xclass: 'here.view.home.HomeContainer'
 						},
 						{
-							xtype : 'button',
-							title : '发布',
-							iconCls : 'compose'
+							xclass : 'here.view.publish.FirstView'
 						},
 						{
 							xclass: 'here.view.my.MyContainer'
