980e38d0309741117fb84caa134916f286ac4626
William <1530358579@qq.com>
2017-12-22 08:37:29 +0800

    定位优化，加载周围点位置优化

--- app/controller/MenuTabController.js
+++ app/controller/MenuTabController.js
@@ -10,9 +10,8 @@ Ext.define('here.controller.MenuTabController', {
         },
         refs: {
 
-            menuTabView : '#menuTabView',
-            locationViewMap : '#locationViewMap'
-        }
+            menuTabView : '#menuTabView'
+     }
     },
 
     menuChange : function(menuTab, value, oldValue, eOpts ){
@@ -23,50 +22,12 @@ Ext.define('here.controller.MenuTabController', {
                 // alert(1);
                 break;
             case 'publishMenu':
-                me.loadNearbyLocation();
                 break;
             case 'myMenu':
                 // alert(3);
                 break;
 
         }
-    },
-
-    // 加载周边位置
-    loadNearbyLocation : function(){
-        var me = this;
-
-        // 获取位置点视图
-        var locationView = me.getLocationViewMap();
-        var myLocation = here.util.LocationUtil.getMyLocation();
-
-
-        // 添加我的位置周边位置
-        Ext.Ajax.request({
-            url: window.localStorage.getItem("SERVER_URL")+'/locationController/getNearbyLocationList',
-            useDefaultXhrHeader: false,
-            params: {
-                lng:myLocation.longitude,
-                lat:myLocation.latitude,
-                radius : 1000
-            },
-            method : "POST",
-            success: function(response){
-                var responseJSON = Ext.JSON.decode(response.responseText,true);
-                if(responseJSON.pointLocationDtoList){
-                    Ext.Array.each(responseJSON.pointLocationDtoList,function(item, index, length){
-                        if(item['lng'] != locationView.getCenter().lng || item['lat'] != locationView.getCenter().lat) {
-                            locationView.addPoint(item['lng'], item['lat'], item, locationView, locationView.getMap());
-                        }
-                    });
-                }
-            },
-            failure : function(response) {
-                Ext.Msg.alert('提示', '加载周围点位置出错！', Ext.emptyFn);
-
-            }
-        });
-
     }
 });
 
--- app/view/map/LocationViewMap.js
+++ app/view/map/LocationViewMap.js
@@ -79,8 +79,46 @@ Ext.define('here.view.map.LocationViewMap', {
             lat:myLocation.latitude
         };
         me.setCenter(center);
+        me.onLocate();
+    },
+    onLocate : function () {
+        var me = this;
+        me.callParent();
+        me.loadNearbyLocation();
+    },
 
-        // 添加我的位置标注
-        me.addMyPoint(me.getCenter().lng, me.getCenter().lat);
+    // 加载周边位置
+    loadNearbyLocation : function(){
+        var me = this;
+
+        var myLocation = here.util.LocationUtil.getMyLocation();
+
+
+        // 添加我的位置周边位置
+        Ext.Ajax.request({
+            url: window.localStorage.getItem("SERVER_URL")+'/locationController/getNearbyLocationList',
+            useDefaultXhrHeader: false,
+            params: {
+                lng:myLocation.longitude,
+                lat:myLocation.latitude,
+                radius : 1000
+            },
+            method : "POST",
+            success: function(response){
+                var responseJSON = Ext.JSON.decode(response.responseText,true);
+                if(responseJSON.pointLocationDtoList){
+                    Ext.Array.each(responseJSON.pointLocationDtoList,function(item, index, length){
+                        if(item['lng'] != myLocation.longitude || item['lat'] != myLocation.latitude) {
+                            me.addPoint(item['lng'], item['lat'], item, me, me.getMap());
+                        }
+                    });
+                }
+            },
+            failure : function(response) {
+                Ext.Msg.alert('提示', '加载周围点位置出错！', Ext.emptyFn);
+
+            }
+        });
+
     }
 });
