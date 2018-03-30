143a00e324b4befc7099a8bbe8dfd2095093ad70
William <1530358579@qq.com>
2018-03-30 08:54:10 +0800

    用户登录测试

--- app/controller/login/LoginViewController.js
+++ app/controller/login/LoginViewController.js
@@ -1,6 +1,6 @@
 Ext.define('here.controller.login.LoginViewController', {
     extend: 'Ext.app.Controller',
-    requires : [],
+    requires : ['here.util.EncryptUtil','here.util.PostUtil'],
     config: {
         control: {
             confirmLoginButton : {
@@ -13,8 +13,15 @@ Ext.define('here.controller.login.LoginViewController', {
         }
     },
     confirmLogin : function () {
-        Ext.Viewport.animateActiveItem(1,'fade');
-
+        var me = this;
+        here.util.PostUtil.post('/userController/validateUserLogin', {
+                    userName: 'admin',
+                    password: here.util.EncryptUtil.encrypt("123456")
+                },
+                function (responseJSON) {
+                    Ext.Viewport.animateActiveItem(1,'fade');
+                }
+        );
     }
 
 });
