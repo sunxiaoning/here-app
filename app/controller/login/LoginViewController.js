e9391d83ea0c4f0f486e3225a4f962cf39ea6235
孙效宁 <sunxiaoning@kjtpay.com.cn>
2018-05-10 13:47:51 +0800

    用户登陆

--- app/controller/login/LoginViewController.js
+++ app/controller/login/LoginViewController.js
@@ -6,7 +6,6 @@ Ext.define('here.controller.login.LoginViewController', {
             confirmLoginButton : {
                 tap : 'confirmLogin'
             }
-
         },
         refs: {
             confirmLoginButton : '#confirmLoginButton'
@@ -16,18 +15,27 @@ Ext.define('here.controller.login.LoginViewController', {
         var me = this;
         var userName = Ext.ComponentQuery.query("#loginView textfield[name=userName]")[0].getValue();
         var password = Ext.ComponentQuery.query("#loginView passwordfield[name=password]")[0].getValue();
-        here.util.PostUtil.post('/userController/validateUserLogin', {
+
+        // 加密用户密码
+        here.util.EncryptUtil.encrypt(password,function (encryptedPassword) {
+
+            // 发送登陆请求
+            here.util.PostUtil.postWithSign('/userGrpcController/security/validateUserLogin', {
                     userName: userName,
-                    password: here.util.EncryptUtil.encrypt(password)
+                    password: encryptedPassword
                 },
                 function (responseJSON) {
+
+                    // 登陆成功保存用户登陆token信息
                     SYSTEM_CONFIG['TOKEN'] = responseJSON.token;
                     Ext.Viewport.animateActiveItem(1,'fade');
                 },
                 function () {
                     window.plugins.toast.showShortBottom('用户名或密码错误，登录失败！');
                 }
-        );
+            );
+        });
+
     }
 
 });
