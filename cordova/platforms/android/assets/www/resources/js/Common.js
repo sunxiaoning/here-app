25fe37861ddd9ac4a06ece3aa3a0597a4137e9cf
William <1530358579@qq.com>
2018-05-12 22:44:19 +0800

    app framework

---
+++ cordova/platforms/android/assets/www/resources/js/Common.js
@@ -0,0 +1,11 @@
+
+// 系统配置
+var SYSTEM_CONFIG = {
+    ENCRYPT_KEY : 'mhVwtpvoBqQAqz/d', // 和服务端传输数据时的加密钥匙
+    SERVER_URL : 'http://39.106.122.85:8080', // 服务端主机和端口号
+    GET_RSA_KEY_PAIR_URL : '/encryptKeyGeneratorController/getRsaKeyPair',
+    GET_AES_KEY_URL : '/encryptKeyGeneratorController/getEncryptKey',
+    SIGN_URL : '/signController/getRequestParamSign',
+    CLIENT_IP : '127.0.0.1',// 客户端IP地址,
+    TOKEN : ''
+}
